c76b4ac9671aaf0ca747f70120f69f39b5aa4ae2
Alexander Brhane <brhanealexander@gmail.com>
2025-01-20 17:48:09 -0600

    The real4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ function Hero() {
         <div className=' flex mt-8 w-full justify-center items-center'>
         <h1
                         style={{borderRadius: "54% 46% 37% 63% / 39% 60% 40% 61% "}} 
-        className='godown -mb-12 w-1/3 p-2 font-bold border-white border-b-2 text-5xl text-white'>
+        className='godown -mb-12 w-1/3 p-2 font-bold border-white border-b-2 text-8xl text-white'>
             Crown You
         </h1>
         </div>
@@ -46,8 +46,8 @@ function Hero() {
                     <img className='h-full' src={hero1} alt="" />
                 </div>
                 <div className='font-reviolaRegular goup mt-8 w-full text-white rounded-b-full h-2/5 overflow-hidden border-b-8 border-white pb-2 justify-center items-center text-center'>
-                    <h1 className='text-xs text-center '>
-                    We are conveniently located in the heart of Calgary, offering a serene and luxurious environment where you can experience our premium services.
+                    <h1 className='text-xs text-center mb-4'>
+                    We are located in the heart of Calgary, offering a serene and luxurious environment where you can experience our premium services.
                     </h1>
                     <button className='m-1 text-black p-1 bg-gray-600 hover:bg-gray-800 shadow-inner border-white border-2 hover:text-white rounded-2xl text-xs'>check address</button>
                 </div>           
@@ -58,11 +58,11 @@ function Hero() {
                 </h1> */}
                 <p 
                 style={{borderRadius: "54% 46% 37% 63% / 39% 60% 40% 61% "}}
-                className='-mt-24 p-12 border-gray-500 border-t-2 border-r-2  text-white text-xl text-balance w-full'>Where Excellence Meets Elegance. Experience unmatched precision and style, tailored just for you. Your journey to sophistication begins with us.</p>
+                className='-mt-24 p-12 border-gray-500 border-t-2 border-r-2  text-white text-3xl font-bold text-balance w-full'>Where Excellence Meets Elegance. Experience unmatched precision and style, tailored just for you. Your journey to sophistication begins with us.</p>
             </div>
             <div className='font-reviolaRegular flex flex-col w-1/4 h-full px-3'>
                 <div className='godown text-white mb-8 w-full rounded-t-full h-2/5 overflow-hidden border-t-8 border-white pt-2'>
-                    <h1 className='mt-24 text-xs text-center' >
+                    <h1 className='mt-24 text-xs text-center px-2 mb-4' >
                     Call us at (403) 555-0123 to book an appointment or for any inquiries. Our friendly staff is ready to assist you.
                     </h1>
                     <button className='m-1 text-black p-1 bg-gray-600 hover:bg-gray-800 shadow-inner border-white border-2 hover:text-white rounded-2xl text-xs'>Call us</button>
